f4678d3ce7c7d9406bfc3f2451be1f1c5d60b99f
ngwatso <ngwatson79@yahoo.com>
2021-01-22 21:37:58 -0500

    added state/componentDidMount to PlantList.js

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -2,38 +2,62 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
-  // add state with a property called "plants" - initialize as an empty array
+	// TODO add state with a property called "plants" - initialize as an empty array
+	constructor() {
+		super();
+		this.state = { plants: [] };
+	}
+	//   TODO - when the component mounts:
+	//   TODO - fetch data from the server endpoint - http://localhost:3333/plants
+	//   TODO - set the returned plants array to this.state.plants
 
-  // when the component mounts:
-  //   - fetch data from the server endpoint - http://localhost:3333/plants
-  //   - set the returned plants array to this.state.plants
+	componentDidMount() {
+		axios.get("http://localhost:3333/plants")
+			.then((res) => {
+				console.log(`NW: PlantList.js: CDM: res:`, res.data);
+				this.setState({ plants: [res.data] });
+			})
+			.catch((err) => console.error(`ERROR PULLING DATA`, err));
+	}
 
-  /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
-  render() {
-    return (
-      <main className="plant-list">
-        {this.state?.plants?.map((plant) => (
-          <div className="plant-card" key={plant.id} data-testid="plant-card">
-            <img className="plant-image" src={plant.img} alt={plant.name} />
-            <div className="plant-details">
-              <h2 className="plant-name">{plant.name}</h2>
-              <p className="plant-scientific-name">{plant.scientificName}</p>
-              <p>{plant.description}</p>
-              <div className="plant-bottom-row">
-                <p>${plant.price}</p>
-                <p>☀️ {plant.light}</p>
-                <p>💦 {plant.watering}x/month</p>
-              </div>
-              <button
-                className="plant-button"
-                onClick={() => this.props.addToCart(plant)}
-              >
-                Add to cart
-              </button>
-            </div>
-          </div>
-        ))}
-      </main>
-    );
-  }
+	/*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
+	render() {
+		return (
+			<main className="plant-list">
+				{this.state?.plants?.map((plant) => (
+					<div
+						className="plant-card"
+						key={plant.id}
+						data-testid="plant-card"
+					>
+						<img
+							className="plant-image"
+							src={plant.img}
+							alt={plant.name}
+						/>
+						<div className="plant-details">
+							<h2 className="plant-name">{plant.name}</h2>
+							<p className="plant-scientific-name">
+								{plant.scientificName}
+							</p>
+							<p>{plant.description}</p>
+							<div className="plant-bottom-row">
+								<p>${plant.price}</p>
+								<p>☀️ {plant.light}</p>
+								<p>💦 {plant.watering}x/month</p>
+							</div>
+							<button
+								className="plant-button"
+								onClick={() =>
+									this.props.addToCart(plant)
+								}
+							>
+								Add to cart
+							</button>
+						</div>
+					</div>
+				))}
+			</main>
+		);
+	}
 }
